f6c4c6f0f7f9c8ca093a0f1c5df48e53749c8abe
Nico <nicosetzes@gmail.com>
2023-09-21 11:12:05 -0300

    V1.6 I've replaced everything that had jQuery with vanilla JS (for the carousel), and made some more twerks to it.

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,58 @@
-const carousel = document.querySelector("#carousel");
-if (window.matchMedia("(min-width: 992px)").matches) {
-  let newCarousel = new bootstrap.Carousel(carousel, {
-    interval: false,
-    wrap: false,
-  });
-  let carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  let cardWidth = $(".carousel-item").width();
-  let scrollPosition = 0;
-  $("#carousel .carousel-control-next").on("click", function () {
-    console.log("fw");
-    if (scrollPosition < carouselWidth - cardWidth * 3) {
-      scrollPosition += cardWidth;
-      $("#carousel .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-  $("#carousel .carousel-control-prev").on("click", function () {
-    console.log("prev");
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $("#carousel .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-} else {
-  $(carousel).addClass("slide");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector("#carousel");
+
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    let newCarousel = new bootstrap.Carousel(carousel, {
+      interval: false, // Disable automatic sliding
+      wrap: false, // Prevent wrapping at the end
+    });
+
+    let carouselWidth = document.querySelector(".carousel-inner").scrollWidth;
+    let cardWidth = document.querySelector(".carousel-item").offsetWidth;
+    let scrollPosition = 0;
+
+    document.querySelector("#carousel .carousel-control-prev").style.display =
+      "none"; // Escondo la flecha de retroceso de movida, ya que arranco en la primera slide //
+
+    document
+      .querySelector("#carousel .carousel-control-next")
+      .addEventListener("click", function () {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+          scrollPosition += cardWidth * 3; // Avanzo de a 3 fotos a la vez
+          document
+            .querySelector("#carousel .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+          if (scrollPosition > 0)
+            document.querySelector(
+              "#carousel .carousel-control-prev"
+            ).style.display = "flex"; // Vuelvo a mostrar la flecha de retroceso si la posición NO es la 0 //
+          if (scrollPosition >= carouselWidth - cardWidth * 4) {
+            document.querySelector(
+              "#carousel .carousel-control-next"
+            ).style.display = "none"; // Desaparece la flecha para adelantar si llegué al final del carrusel //
+          }
+        }
+      });
+
+    document
+      .querySelector("#carousel .carousel-control-prev")
+      .addEventListener("click", function () {
+        if (scrollPosition > 0) {
+          scrollPosition -= cardWidth * 3; // Retrocedo de a 3 fotos a la vez
+          document
+            .querySelector("#carousel .carousel-inner")
+            .scroll({ left: scrollPosition, behavior: "smooth" });
+          if (scrollPosition <= 0)
+            document.querySelector(
+              "#carousel .carousel-control-prev"
+            ).style.display = "none"; // Escondo la flecha de retroceso //
+        }
+        if (scrollPosition < carouselWidth - cardWidth * 4)
+          document.querySelector(
+            "#carousel .carousel-control-next"
+          ).style.display = "flex"; // Reaparece la flecha para adelantar si se puede ir hacia adelante //
+      });
+  } else {
+    carousel.classList.add("slide");
+  }
+});
